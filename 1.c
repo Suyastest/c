aee95a8d06f631edcfb7df8492f5fb83d5f00ca8
Suyastest <166650966+Suyastest@users.noreply.github.com>
2024-04-11 00:48:54 +0530

    check

---
+++ 1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>    
+int main(){    
+int radius;    
+printf("enter a radius: ");    
+scanf("%d",&radius);    
+float circumference = 2*3.14*radius;
+printf("circumference of circle is: %f ", circumference);    
+return 0;  
+}
